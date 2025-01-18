91e5ca1e277d399eac589cc3b1a2748a6d4e4665
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-19 00:01:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.getElementById('subscribe-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const email = document.getElementById('email').value;
+  
+  if (email) {
+    console.log(`New subscription: ${email}`);
+    alert('Thanks for subscribing!');
+    document.getElementById('email').value = '';
+  } else {
+    alert('Please enter a valid email.');
+  }
+});
 
